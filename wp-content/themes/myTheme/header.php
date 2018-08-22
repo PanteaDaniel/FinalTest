2d9489fd1da5587571eb961b0f21facc99df94e6
Daniel Pantea <danie@Daneil.com>
2018-08-22 10:46:50 +0300

    Am creeat o tema

---
+++ wp-content/themes/myTheme/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title><?php echo get_bloginfo( 'title' ); ?></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link href="<?php echo get_template_directory_uri();?>/css/style.css" rel="stylesheet">
+
+	<?php wp_head();?>
+
+</head>
+<body>
